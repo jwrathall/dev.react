3ac30de55271764e232e02c9b80664ad98e4376b
jwrathall <wrathallj@gmail.com>
2016-03-24 16:23:30 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var open = require('gulp-open'); //open url in browser
+var browserify = require('browserify'); //js bundler
+var reactify = require('reactify'); //transform jsx to js
+var source = require('vinyl-source-stream'); //use conventional text streams with gulp
+
+var config = {
+    port: 3000,
+    devBaseUrl: 'http://localhost',
+    paths:{
+        html:'./src/*.html',
+        dist: './dist',
+        js: './src/**/*.js',
+        mainJs: './src/main.js'
+    }
+}
+
+//start local server
+
+gulp.task('connect', function(){
+    connect.server({
+        root: ['dist'],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true
+    })
+});
+
+gulp.task('open',['connect'],function(){
+   gulp.src('dist/index.html')
+    .pipe(open({uri: config.devBaseUrl+':'+config.port+'/'})); 
+});
+
+gulp.task('html', function(){
+   gulp.src(config.paths.html)
+   .pipe(gulp.dest(config.paths.dist))
+   .pipe(connect.reload()); 
+});
+
+gulp.task('js',function(){
+   browserify(config.paths.mainJs) 
+    .transform(reactify)
+    .bundle()
+    .on('error', console.error.bind(console))
+    .pipe(source('dist.js'))
+    .pipe(gulp.dest(config.paths.dist + '/scripts'))
+    .pipe(connect.reload());
+    
+});
+
+gulp.task('watch',function(){
+   gulp.watch(config.paths.html,['html']); 
+   gulp.watch(config.paths.js,['js']); 
+});
+
+gulp.task('default',['html','js', 'open','watch']);
